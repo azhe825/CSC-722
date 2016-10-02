bfbfce0e781bb65a9ee2c1745fba87d56bfc15c9
Yu <YUXZ1@legal.regn.net>
2016-10-02 09:34:12 -0400

    hw2

--- project/src/hw2.py
+++ project/src/hw2.py
@@ -16,64 +16,66 @@ def loadData():
     label=np.array(label)
     return data,label
 
-def exp_folds(model):
-    if model=="SVM":
-        model=SVM
-    elif model=="MLP":
-        model=MLP
-    data,label=loadData()
-    folds=[10,25,100]
-    best={}
-    F1={}
-    mlp=model()
-    for fold in folds:
-        best[fold]={}
-        F1[fold]={}
-
-        repeats=10
-        result=[]
-        for i in xrange(repeats):
-            tmp=mlp.crossval(data,label,fold=fold)
-            result.append(tmp)
-        best[fold]=result
-    set_trace()
+# def exp_folds(model):
+#     if model=="SVM":
+#         model=SVM
+#     elif model=="MLP":
+#         model=MLP
+#     data,label=loadData()
+#     folds=[10,25,100]
+#     best={}
+#     F1={}
+#     mlp=model()
+#     for fold in folds:
+#         best[fold]={}
+#         F1[fold]={}
+#
+#         repeats=10
+#         result=[]
+#         for i in xrange(repeats):
+#             tmp=mlp.crossval(data,label,fold=fold)
+#             result.append(tmp)
+#         best[fold]=result
+#     set_trace()
 
 
 
 def exp_mlp():
     data, label = loadData()
-    folds = 100
+    folds = [5,10,25,100]
     activations = ['identity', 'logistic', 'tanh', 'relu']
     max_iters = [200, 500, 1000, 2000]
     hidden_layer = [(5, 2), (10, 4), (10), (20), (30), (40), (80)]
 
     Fbest=0
-    for act in activations:
-        for epoch in max_iters:
-            for hid in hidden_layer:
-                mlp=MLP(activation=act,max_iter=epoch, hidden_layer_sizes=hid)
-                F1=mlp.crossval(data,label,folds)
-                if F1>Fbest:
-                    Fbest=F1
-                    best=(act,epoch,hid)
+    for fold in folds:
+        for act in activations:
+            for epoch in max_iters:
+                for hid in hidden_layer:
+                    mlp=MLP(activation=act,max_iter=epoch, hidden_layer_sizes=hid)
+                    F1=mlp.crossval(data,label,fold)
+                    if F1>Fbest:
+                        Fbest=F1
+                        best=(act,epoch,hid,fold)
     set_trace()
 
 def exp_svm():
     data, label = loadData()
-    folds = 100
+    folds = [5,10,25,100]
     kernels = ['linear', 'poly', 'rbf', 'sigmoid']
     max_iters = [200, 500, 1000, 2000, -1]
     Cs = [1, 0.5, 0.2, 0.1, 2, 5, 10]
 
     Fbest = 0
-    for kernel in kernels:
-        for epoch in max_iters:
-            for C in Cs:
-                svm = SVM(kernel=kernel, max_iter=epoch, C=C)
-                F1 = svm.crossval(data, label, folds)
-                if F1 > Fbest:
-                    Fbest = F1
-                    best = (kernel, epoch, C)
+    for fold in folds:
+        for kernel in kernels:
+            for epoch in max_iters:
+                for C in Cs:
+                    svm = SVM(kernel=kernel, max_iter=epoch, C=C)
+                    F1 = svm.crossval(data, label, fold)
+                    if F1 > Fbest:
+                        Fbest = F1
+                        best = (kernel, epoch, C, fold)
     set_trace()
 
 
