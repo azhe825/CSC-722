a8beddce3b35650ca672945c6f36bd07b46769a7
Zhe Yu <zyu9@ncsu.edu>
2016-10-17 20:41:41 -0400

    n

--- project/src/test.py
+++ project/src/test.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 from demos import cmd
 from sklearn import svm
-from random import random
+from random import random,seed
 from collections import Counter
 from pdb import set_trace
 from mpl_toolkits.mplot3d import Axes3D
@@ -93,6 +93,91 @@ def scatter_points():
 
     plt.savefig("../figure/4.png")
 
+
+def scatter_points2(s):
+    min=0
+    max=6
+    seed(s)
+    num=100
+
+    x_p = []
+    x_n = []
+    x_na = []
+    y_na = []
+    y_p = []
+    y_n = []
+    can = []
+
+    for i in xrange(num):
+        can.append([random()*max,random()*max])
+        if sum(can[i])>2:
+            x_n.append(can[i][0])
+            y_n.append(can[i][1])
+        else:
+            x_p.append(can[i][0])
+            y_p.append(can[i][1])
+
+
+    plt.figure(1)
+    plt.scatter(x_p,y_p,marker='+', s=250, edgecolors='blue', linewidths=3)
+    plt.scatter(x_n,y_n,marker='x', s=250, edgecolors='red', linewidths=3)
+    plt.plot([0,2],[2,0],color='black', linewidth=3)
+
+    plt.xlim([min,max])
+    plt.ylim([min,max])
+
+    plt.savefig("../figure/train.png")
+
+    pos=len(x_p)
+    a=[sum(c) for c in can]
+    for id in np.argsort(a)[::-1][:pos]:
+        x_na.append(can[id][0])
+        y_na.append(can[id][1])
+
+    plt.figure(2)
+    plt.scatter(x_p,y_p,marker='+', s=250, edgecolors='blue', linewidths=3)
+    plt.scatter(x_na,y_na,marker='x', s=250, edgecolors='red', linewidths=3)
+    plt.plot([0,6],[6,0],color='black', linewidth=3)
+
+    plt.xlim([min,max])
+    plt.ylim([min,max])
+
+    plt.savefig("../figure/train_a.png")
+
+def scatter_points3(s):
+    min=0
+    max=6
+    seed(s)
+    num=100
+
+    x_p = []
+    x_n = []
+    y_p = []
+    y_n = []
+
+    for i in xrange(num):
+        can=[random()*max,random()*max]
+        x_n.append(can[0])
+        y_n.append(can[1])
+
+
+    plt.figure(1)
+    plt.scatter(x_n,y_n,marker='.', s=250, color='green', linewidths=3)
+    plt.plot([0,2],[2,0],color='black', linewidth=3)
+
+    plt.xlim([min,max])
+    plt.ylim([min,max])
+
+    plt.savefig("../figure/test.png")
+
+    plt.figure(2)
+    plt.scatter(x_n,y_n,marker='.', s=250, color='green', linewidths=3)
+    plt.plot([0,6],[6,0],color='black', linewidth=3)
+
+    plt.xlim([min,max])
+    plt.ylim([min,max])
+
+    plt.savefig("../figure/test_a.png")
 
 if __name__ == "__main__":
     eval(cmd())
